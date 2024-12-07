d50b4c112239d0ab4ffd566e4f299ebde91b22c4
Skrzyqu <michal14.ma@gmail.com>
2024-12-07 20:22:28 +0100

    First-try with Database

--- WordWizzProject/WordWizzApp/models.py
+++ WordWizzProject/WordWizzApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Word(models.Model):
+    polish_word = models.TextField()
+    german_word = models.TextField()
+
+    def __str__(self):
+        return f"{self.polish_word} - {self.german_word}"
--- WordWizzProject/WordWizzApp/views.py
+++ WordWizzProject/WordWizzApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Word
+
+def word_list(request):
+    words = Word.objects.all()
+    return render(request, 'word_list.html', {'words': words})
